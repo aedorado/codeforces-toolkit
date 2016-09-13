d6003e3a18d9bce529fb94925af16dbb522a39d0
Anurag El Dorado <annurag94@gmail.com>
2016-09-13 14:07:51 +0530

    Added contest directory.

--- script.py
+++ script.py
@@ -7,8 +7,8 @@ ext = raw_input('Enter filename extension : ')
 contest_id = contest_id.strip()
 ext = ext.strip()
 
-if not os.path.exists(contest_id):
-	os.mkdir(contest_id)
+if not os.path.exists('contest/' + contest_id):
+	os.mkdir('contest/' + contest_id)
 
 for filename in range(ord('A'), ord('F')):
-	shutil.copy('templates/' + ext, contest_id + '\\' + contest_id + chr(filename) + '.' + ext)
+	shutil.copy('templates/' + ext, 'contest/' + contest_id + '/' + contest_id + chr(filename) + '.' + ext)
